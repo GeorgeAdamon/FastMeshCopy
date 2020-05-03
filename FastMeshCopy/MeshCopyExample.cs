4847dcdc9af46143aa6208a028c8a711002bc19b
GeorgeAdamon <ga.adamon@gmail.com>
2020-05-03 20:03:43 +0100

    Initial Commit

---
+++ FastMeshCopy/MeshCopyExample.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class MeshCopyExample : MonoBehaviour
+{
+    public Mesh inMesh;
+    public Mesh outMesh;
+    private MeshFilter mf;
+
+    private void Start()
+    {
+        mf = GetComponent<MeshFilter>();
+        outMesh = new Mesh();
+        mf.sharedMesh = outMesh;
+    }
+    
+    private void Update()
+     {
+         if (!Input.GetKeyDown(KeyCode.Space)) return;
+         
+         inMesh.CopyTo(ref outMesh);
+     }
+}
+
